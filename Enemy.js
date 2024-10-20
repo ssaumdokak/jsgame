a93774ebf1686517872eee5bd673e2c3772e8acd
dao <146765250+ssaumdokak@users.noreply.github.com>
2024-10-20 15:53:19 +0200

    Create Enemy.js

---
+++ Enemy.js
@@ -0,0 +1,10 @@
+// Enemy.js
+import Pokemon from './Pokemon.js';
+
+class Enemy extends Pokemon {
+    constructor(name, elementIds) {
+        super(name, elementIds);
+    }
+}
+
+export default Enemy;
