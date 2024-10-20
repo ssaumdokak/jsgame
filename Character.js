869aed85e8dd798b4dadf9c5b56157bf81cb83db
dao <146765250+ssaumdokak@users.noreply.github.com>
2024-10-20 15:53:34 +0200

    Create Character.js

---
+++ Character.js
@@ -0,0 +1,10 @@
+// Character.js
+import Pokemon from './Pokemon.js';
+
+class Character extends Pokemon {
+    constructor(name, elementIds) {
+        super(name, elementIds);
+    }
+}
+
+export default Character;
